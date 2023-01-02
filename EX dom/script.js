9b49947b3fcc5597b58719ec1d44bb711cb14295
diaze0 <92938629+diaze0@users.noreply.github.com>
2023-01-02 10:28:39 +0100

    Add files via upload

---
+++ EX dom/script.js
@@ -0,0 +1,14 @@
+
+
+for(var items in document.images){
+document.images[items].src = "22.jpg";
+document.images[items].width = "200";
+document.images[items].height="200";
+
+
+}
+
+
+document.getElementById("p1").innerHTML = "New text!" ;
+document.getElementById("p1").style.color = "blue";
+
